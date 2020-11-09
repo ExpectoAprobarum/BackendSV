544895a4c3ee4a3c2221e079be0c75ce7046598f
tupapiloaleman <andreskammerath@gmail.com>
2020-11-09 17:06:00 -0300

    tests for create game and join game had been implemented

--- src/test_main.py
+++ src/test_main.py
@@ -4,17 +4,24 @@ import pytest
 
 client = TestClient(app)
 
-pytest.users = {1: {"username": "andres", "email": "a@a.com", "password": "12345"}, 2: {"username": "andres2", "email": "a2@a.com", "password": "12345"}}
+pytest.users = {
+    1: {"username": "andres", "email": "a@a.com", "password": "12345"},
+    2: {"username": "andres2", "email": "a2@a.com", "password": "12345"},
+    3: {"username": "andres3", "email": "a3@a.com", "password": "12345"},
+    4: {"username": "andres4", "email": "a4@a.com", "password": "12345"},
+    5: {"username": "andres5", "email": "a5@a.com", "password": "12345"}
+    }
 
 
 def test_create_user():
-    response = client.post(
-        "/users/", headers={},
-        json=pytest.users[1]
-    )
-    assert response.status_code == 200
-    rjson = response.json()
-    assert rjson == {"id": 1, "message": "User created successfully"}
+    for i,u in enumerate(pytest.users.values()):
+        response = client.post(
+            "/users/", headers={},
+            json={"username": u['username'], "email": u['email'], "password": "12345"}
+        )
+        assert response.status_code == 200
+        rjson = response.json()
+        assert rjson == {"id": i+1, "message": "User created successfully"}
     response = client.post(
         "/users/", headers={},
         json=pytest.users[1]
@@ -29,16 +36,44 @@ def test_create_user():
     assert response.json() == {"detail": "Email already registered"}
 
 def test_auth():
-    response = client.post("/auth/token", headers={},
-        json={"username": "andres", "password": "12345"}
-    )
+    for i,u in enumerate(pytest.users.values()):
+        response = client.post("/auth/token", headers={},
+            json={"username": u["username"], "password": "12345"}
+        )
+        assert response.status_code == 200
+        rjson = response.json()
+        assert rjson['token_type'] == 'bearer'
+        u["token"] = rjson['access_token']
+
+def test_create_game():
+    headers = {
+    'Authorization': 'Bearer ' + pytest.users[1]["token"],
+    'Content-Type': 'text/plain'
+    }
+    response = client.post("/games/", headers=headers,
+        json={"name":"Partida 1", "player_amount": 5})
     assert response.status_code == 200
-    rjson = response.json()
-    assert rjson['token_type'] == 'bearer'
-    pytest.users[1]["token"] = rjson['access_token']
+    assert response.json() == {'id': 1, 'message': 'Game created successfully'}
 
 
+def test_join_game():
+    for i,u in enumerate(list(pytest.users.values())[1:]):
+        headers = {
+        'Authorization': 'Bearer ' + u["token"],
+        'Content-Type': 'text/plain'
+        }
+        print('Bearer ' + u["token"])
+        response = client.post("/games/1/join", headers=headers, json={})
+        assert response.status_code == 200
+        assert response.json() == {"message": 'joined successfully'}
+    response = client.post("/games/100/join", headers=headers, json={})
+    assert response.status_code == 404
+    assert response.json() == {"detail": 'Game not found'}
+    response = client.post("/games/1/join", headers=headers, json={})
+    assert response.status_code == 403
+    assert response.json() == {"detail": 'The game is full'}
 
+"""
 def test_get_game():
     headers = {
     'Authorization': 'Bearer ' + pytest.users[1]["token"],
@@ -47,7 +82,7 @@ def test_get_game():
     response = client.get("/games/", headers=headers)
     assert response.status_code == 200
     
-"""
+
 def test_create_games():
     response = client.post(
         "/games/", headers={"X-Token": "coneofsilence"},
